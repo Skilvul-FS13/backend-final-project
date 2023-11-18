d21e54c9ddec858fec808d19cecf25338553ac7c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-18 16:39:00 +0700

    Remove authentication middleware from users route

--- src/router/usersRoute.js
+++ src/router/usersRoute.js
@@ -4,7 +4,7 @@ const { getAllUser, getUserById, register, login } = require('../controller/user
 const auth = require('../middleware/auth');
 const route = express.Router();
 
-route.get('/', auth, getAllUser);
+route.get('/', getAllUser);
 route.get('/:id', getUserById);
 route.post('/', register);
 route.post('/login', login);
