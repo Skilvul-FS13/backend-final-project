9b84a7887510e24c1ee550233171c4d32c5d8c6c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-19 15:11:46 +0700

    Add editUser route to usersRoute.js

--- src/router/usersRoute.js
+++ src/router/usersRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const { getAllUser, getUserById, register, login } = require('../controller/userController');
+const { getAllUser, getUserById, register, login, editUser } = require('../controller/userController');
 const auth = require('../middleware/auth');
 const route = express.Router();
 
 route.get('/', getAllUser);
 route.get('/:id', getUserById);
+route.put('/:id', auth, editUser);
 route.post('/', register);
 route.post('/login', login);
 
