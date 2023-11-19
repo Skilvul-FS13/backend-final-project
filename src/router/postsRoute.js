c39d7b6ea37bcb69eb0522e997dd008af42f3d5e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-19 18:52:28 +0700

    Add authentication to create new posts

--- src/router/postsRoute.js
+++ src/router/postsRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const { getAllPost, getPostById, deletePost } = require('../controller/postController');
+const { getAllPost, getPostById, deletePost, addPost } = require('../controller/postController');
 const auth = require('../middleware/auth');
 const route = express.Router();
 
 route.get('/', getAllPost);
 route.get('/:id', getPostById);
-// route.post('/', addPost);
+route.post('/', auth, addPost);
 route.delete('/:id', auth, deletePost);
 
 module.exports = route;
