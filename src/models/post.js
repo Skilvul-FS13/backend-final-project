fbf83d1c0ef925be8e5b061d0e0a340c32919337
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-19 15:11:32 +0700

    Add associations for Likes and Comments in Post model

--- src/models/post.js
+++ src/models/post.js
@@ -1,6 +1,8 @@
 'use strict';
 const { Model } = require('sequelize');
 const { User } = require('./user');
+const { Likes } = require('./likes');
+const { Comments } = require('./comments');
 module.exports = (sequelize, DataTypes) => {
   class Post extends Model {
     /**
@@ -10,18 +12,33 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Post.belongsTo(models.User, { foreignKey: 'userId' });
+      Post.belongsTo(models.User);
+      Post.hasMany(models.Likes);
+      Post.hasMany(models.Comments);
     }
   }
   Post.init(
     {
       post: DataTypes.STRING,
       image: DataTypes.STRING,
-      likes: DataTypes.INTEGER,
-      UserId: {
+      likeId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: Likes,
+          key: 'id',
+        },
+      },
+      userId: {
         type: DataTypes.INTEGER,
         references: {
           model: User,
+          key: 'id',
+        },
+      },
+      commentId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: Comments,
           key: 'id',
         },
       },
