9918b3f84889d10fb18b0a366e4ac0c1e0282847
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-19 15:11:53 +0700

    Add seed data for Likes table

---
+++ src/seeders/20231119025320-seed-likes.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert('Likes', [
+      {
+        postId: 1,
+        userId: 2,
+      },
+      {
+        postId: 1,
+        userId: 3,
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Likes', null, {});
+  },
+};
