68464ae18defcda079c310d948a4c987766cad8f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-19 19:55:58 +0700

    Add News model with associations

---
+++ src/models/news.js
@@ -0,0 +1,42 @@
+'use strict';
+const { Model } = require('sequelize');
+const { Categories } = require('./categories');
+const { User } = require('./user');
+module.exports = (sequelize, DataTypes) => {
+  class News extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  News.init(
+    {
+      title: DataTypes.STRING,
+      description: DataTypes.STRING,
+      image: DataTypes.STRING,
+      userId: {
+        type: DataTypes.STRING,
+        references: {
+          model: User,
+          key: 'id',
+        },
+      },
+      categoryId: {
+        type: DataTypes.STRING,
+        references: {
+          model: Categories,
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'News',
+    }
+  );
+  return News;
+};
