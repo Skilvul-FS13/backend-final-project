87975debe43cdc80c46091b366c4cac7d0425e10
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-20 20:25:31 +0700

    Update database configuration

--- src/config/config.js
+++ src/config/config.js
@@ -17,11 +17,11 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: 'avnadmin',
-    password: 'AVNS_RQOtb8KTSiixzGk93RK',
-    database: 'defaultdb',
-    host: 'mysql-16949196-beckersteve99-77d0.a.aivencloud.com',
-    port: 25222,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
   },
 };
