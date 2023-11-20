defcbcb823b920a67631018d43b15deea0a0b701
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-20 13:02:44 +0700

    Add delete and edit comment routes

--- src/router/commentsRoute.js
+++ src/router/commentsRoute.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
 const auth = require('../middleware/auth');
-const { getAllComments, getCommentById, addComment } = require('../controller/commentController');
+const { getAllComments, getCommentById, addComment, deleteComment, editComment } = require('../controller/commentController');
 const route = express.Router();
 
 route.get('/', getAllComments);
 route.get('/:id', getCommentById);
 route.post('/', auth, addComment);
-// route.put('/:id', auth, );
-// route.delete('/:id', auth, );
+route.put('/:id', auth, editComment);
+route.delete('/:id', auth, deleteComment);
 
 module.exports = route;
