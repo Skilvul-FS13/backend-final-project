8c9ca2d61eab838d9bbcf6e454aea01e500f9fea
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-20 14:54:30 +0700

    Update user model associations

--- src/models/user.js
+++ src/models/user.js
@@ -12,7 +12,8 @@ module.exports = (sequelize, DataTypes) => {
       User.hasMany(models.Post, { foreignKey: 'userId' });
       User.hasMany(models.Likes, { foreignKey: 'userId' });
       User.hasMany(models.Comments, { foreignKey: 'userId' });
-      User.hasMany(models.Signature, { foreignKey: 'userId' });
+      User.hasMany(models.Signatures, { foreignKey: 'userId' });
+      User.hasMany(models.News, { foreignKey: 'userId' });
       User.hasMany(models.Petitions, { foreignKey: 'userId' });
     }
   }
