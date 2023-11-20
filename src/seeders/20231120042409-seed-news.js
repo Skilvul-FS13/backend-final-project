6a0a561053725e2dce40ea329dd09383e1b76998
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-20 13:03:00 +0700

    Add seeders for categories, news, signatures, and petitions

---
+++ src/seeders/20231120042409-seed-news.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert('Comments', [
+      {
+        title: 'Ini adalah berita pertama',
+        description: 'ini deskripsi dari berita pertama',
+        image: '',
+        userId: 2,
+        categoryId: 1,
+      },
+      {
+        title: 'Ini adalah berita kedua',
+        description: 'ini deskripsi dari berita kedua',
+        image: '',
+        userId: 1,
+        categoryId: 2,
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
---
+++ src/seeders/20231120042417-seed-signatures.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert('Signatures', [
+      {
+        petitionId: 2,
+        userId: 2,
+      },
+      {
+        petitionId: 1,
+        userId: 3,
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
