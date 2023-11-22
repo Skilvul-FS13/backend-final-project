54249c048818ddb7fa5c02e9fd230a56e1f2ee9c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-22 16:14:53 +0700

    Add CORS configuration

--- src/app.js
+++ src/app.js
@@ -18,6 +18,14 @@ const connectToDB = async () => {
   }
 };
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 connectToDB();
 
 const PORT = process.env.PORT || 3000;
