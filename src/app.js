56ceedb43711bdec154f2471e1cc16cb4a0c6759
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-12-01 17:57:23 +0700

    Update CORS origin URL

--- src/app.js
+++ src/app.js
@@ -8,12 +8,12 @@ const app = express();
 app.use(cors());
 
 // Enable CORS for all routes
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://fe-greenworldaware.vercel.ap');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 const connectToDB = async () => {
   try {
