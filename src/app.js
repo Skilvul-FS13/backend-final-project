e864f7f441e7af6df2f64112a0771ab7eb32479c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-12-01 19:00:49 +0700

    Remove commented out CORS code

--- src/app.js
+++ src/app.js
@@ -7,14 +7,6 @@ const app = express();
 
 app.use(cors());
 
-// Enable CORS for all routes
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'localhost');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
-
 const connectToDB = async () => {
   try {
     await sequelize.authenticate();
