2923205bed489512ee2e300f85969faae873966a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-12-01 18:41:16 +0700

    Fix CORS origin in app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 // Enable CORS for all routes
 // app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://fe-greenworldaware.vercel.ap');
+//   res.header('Access-Control-Allow-Origin', 'localhost');
 //   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 //   res.header('Access-Control-Allow-Headers', 'Content-Type');
 //   next();
