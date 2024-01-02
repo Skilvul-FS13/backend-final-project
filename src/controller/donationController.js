e5afed3545cefd7ee869319ae84634cdc4e46dc6
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-01-02 17:00:07 +0700

    Refactor donationGetaway function to handle errors

--- src/controller/donationController.js
+++ src/controller/donationController.js
@@ -4,37 +4,43 @@ const { User, Donations } = require('../models');
 const midtransClient = require('midtrans-client');
 
 const donationGetaway = async (req, res) => {
-  const data = req.body;
+  try {
+    const data = req.body;
 
-  let snap = new midtransClient.Snap({
-    isProduction: false,
-    serverKey: process.env.MIDTRANS_SERVER_KEY,
-    clientKey: process.env.MIDTRANS_CLIENT_KEY,
-  });
-
-  let parameter = {
-    transaction_details: {
-      order_id: 'order-id-node-' + Math.round(new Date().getTime() / 1000),
-      gross_amount: data.donation_amount,
-    },
-    credit_card: {
-      secure: true,
-    },
-    customer_details: {
-      first_name: data.firstName,
-      last_name: data.lastName,
-      email: data.email,
-      phone: data.phone,
-    },
-  };
-
-  snap.createTransaction(parameter).then((transaction) => {
-    let transactionToken = transaction.token;
-    res.status(201).json({
-      data: transactionToken,
+    let snap = new midtransClient.Snap({
+      isProduction: false,
+      serverKey: process.env.MIDTRANS_SERVER_KEY,
+      clientKey: process.env.MIDTRANS_CLIENT_KEY,
     });
-    console.log('Token: ', transactionToken);
-  });
+
+    let parameter = {
+      transaction_details: {
+        order_id: 'order-id-node-' + Math.round(new Date().getTime() / 1000),
+        gross_amount: data.donation_amount,
+      },
+      credit_card: {
+        secure: true,
+      },
+      customer_details: {
+        first_name: data.firstName,
+        last_name: data.lastName,
+        email: data.email,
+        phone: data.phone,
+      },
+    };
+
+    snap.createTransaction(parameter).then((transaction) => {
+      let transactionToken = transaction.token;
+      res.status(201).json({
+        data: transactionToken,
+      });
+      console.log('Token: ', transactionToken);
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
 module.exports = { donationGetaway };
